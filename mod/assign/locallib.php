a5f4d6a1d1a4c988672d0d8dd7f59048f673b938
Gareth J Barnard <gjbarnard@gmail.com>
2013-03-28 09:41:01 +0000

    MDL-37490 mod_assign: Add setting to show the due date on the course page.

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -500,7 +500,16 @@ class assign {
         $update->requiresubmissionstatement = $formdata->requiresubmissionstatement;
         $update->sendnotifications = $formdata->sendnotifications;
         $update->sendlatenotifications = $formdata->sendlatenotifications;
-        $update->duedate = $formdata->duedate;
+        if (empty($formdata->duedateenable)) {
+            $update->duedate = 0;
+        } else {
+            $update->duedate = $formdata->duedate;
+        }
+        if (empty($formdata->displayduedate)) {
+            $update->displayduedate = 0;
+        } else {
+            $update->displayduedate = $formdata->displayduedate;
+        }
         $update->cutoffdate = $formdata->cutoffdate;
         $update->allowsubmissionsfromdate = $formdata->allowsubmissionsfromdate;
         $update->grade = $formdata->grade;
@@ -814,7 +823,16 @@ class assign {
         $update->requiresubmissionstatement = $formdata->requiresubmissionstatement;
         $update->sendnotifications = $formdata->sendnotifications;
         $update->sendlatenotifications = $formdata->sendlatenotifications;
-        $update->duedate = $formdata->duedate;
+        if (empty($formdata->duedateenable)) {
+            $update->duedate = 0;
+        } else {
+            $update->duedate = $formdata->duedate;
+        }
+        if (empty($formdata->displayduedate)) {
+            $update->displayduedate = 0;
+        } else {
+            $update->displayduedate = $formdata->displayduedate;
+        }
         $update->cutoffdate = $formdata->cutoffdate;
         $update->allowsubmissionsfromdate = $formdata->allowsubmissionsfromdate;
         $update->grade = $formdata->grade;
