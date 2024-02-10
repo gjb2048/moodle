8a72f2299e975370cfa0e370ef70d5ab4e520c5a
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2024-02-10 13:02:09 +0000

    Improvements.

--- admin/settings/h5p.php
+++ admin/settings/h5p.php
@@ -50,6 +50,9 @@ if (!empty($defaulth5plib)) {
         PARAM_NOTAGS
     );
     $setting->set_updatedcallback(function () {
+        global $CFG;
+        // Enables use of file_storage contstants.
+        require_once($CFG->dirroot.'/h5p/h5plib/v126/joubel/core/h5p-file-storage.interface.php');
         \core_h5p\file_storage::generate_custom_styles();
     });
     $settings->add($setting);
